cea0767b95186bce40e6dd70f28af5a23ff82ade
Reddity79 <60738361+Redddity79@users.noreply.github.com>
2021-01-04 04:40:12 +0300

    Add files via upload

--- paper-cranes-bot.py
+++ paper-cranes-bot.py
@@ -8,6 +8,7 @@ import json
 import random
 from aiogram.utils.emoji import emojize
 from urllib.request import urlopen
+import requests
 
 token = "1447798712:AAHm6uSknzWH1XZijZgWfc6tAPlWWyKtcOE"
 logging.basicConfig(level=logging.INFO)
@@ -22,6 +23,11 @@ main_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton(emojize
 remove = ReplyKeyboardRemove()
 
 commands = ['Catch a crane.', emojize(':envelope: Read a crane from the pocket.'), emojize(':envelope: Read a crane from the pocket.'), 'Set new name.', emojize(":world_map: Set the location."), 'Cancel.']
+
+SECRET_KEY = '$2b$10$FyaFZ3DqdWEHYUch0yU90uL.8prii18XOrTSgfC4L9hSMKzuzX6dq'
+urls = {'users-info': 'https://api.jsonbin.io/b/5ff2711d14be5470601955bc', 'cranes': 'https://api.jsonbin.io/b/5ff27101a2070e409d6dcec0'}
+
+headers = {'Content-Type': 'application/json', 'secret-key': '$2b$10$FyaFZ3DqdWEHYUch0yU90uL.8prii18XOrTSgfC4L9hSMKzuzX6dq', 'versioning': 'false'}
 
 class User(StatesGroup):
 	waiting_for_location = State()
@@ -50,21 +56,20 @@ def gen():
 @dp.message_handler(commands=['start'], state='*')
 async def start(message: types.Message, state: FSMContext):
 	print(message)
-	with open("users-info.txt") as file:
-		users_info = json.load(file)
-	file.close()
+	
+	req = requests.get(urls['users-info'], headers=headers)
+	users_info = json.loads(req.text)
 	
 	if str(message['from']['id']) in users_info.keys():
 		await message.reply("What do you want now?", reply_markup=main_menu)
+		requests.put(urls['users-info'], json=users_info, headers=headers)
 		await state.finish()
 		return
 	else:
 		users_info[str(message['from']['id'])] = {'country': "none", 'written-cranes': [], 'read-cranes': [], 'nick': "Incognito"}
 		await state.finish()
 	
-	with open("users-info.txt", 'w') as file:
-		file.write(json.dumps(users_info))
-	file.close()
+	requests.put(urls['users-info'], json=users_info, headers=headers)
 	
 	await message.answer(emojize("Hi! I'm crane's coordinator :wink:, and I'll help you to srart. First, send me your location to know your country."), reply_markup = location_request)
 	await User.waiting_for_location.set()
@@ -82,41 +87,39 @@ async def set_state(message: types.Message, state: FSMContext):
 		await message.answer(f'You have some {random.choice(["really bad", "good", "great", "low", "high"])} chanses to catch a paper crane....', reply_markup=catch)
 		await Crane.waiting_for_catch.set()
 	if text == emojize(':envelope: Read a crane from the pocket.'):
-		with open("users-info.txt") as file:
-			users_info = json.load(file)
-		file.close()
+		req = requests.get(urls['users-info'], headers=headers)
+		users_info = json.loads(req.text)
 
 		if len(users_info[str(message['from']['id'])]['read-cranes']) == 0:
 			await message.reply(emojize('You haven`t cought any paper crane, try it. :wink:'), reply_markup=main_menu)
+			requests.put(urls['users-info'], json=users_info, headers=headers)
 			return
 		else:
 			read_crane = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Choice from all cranes.')).add(KeyboardButton('Random crane.')).add(KeyboardButton('Cancel.'))
 			await message.answer(emojize('Choose what you need - choice from all or random one? :face_with_monocle:'), reply_markup=read_crane)
+			requests.put(urls['users-info'], json=users_info, headers=headers)
 			await Crane.waiting_for_choice.set()
 	if text == 'Set new name.':
-		with open('users-info.txt') as file:
-			users_info = json.load(file)
-		file.close()
+		req = requests.get(urls['users-info'], headers=headers)
+		users_info = json.loads(req.text)
 		
 		cancel = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton('Cancel.'))
 		
 		await message.answer(f'Send me your new name. (current name: {users_info[str(message["from"]["id"])]["nick"]})', reply_markup=cancel)
+		requests.put(urls['users-info'], json=users_info, headers=headers)
 		await User.waiting_for_name.set()
 	if text == emojize(":world_map: Set the location."):
-		with open('users-info.txt') as file:
-			users_info = json.load(file)
-		file.close()
+		req = requests.get(urls['users-info'], headers=headers)
+		users_info = json.loads(req.text)
 		
 		if users_info[str(message['from']['id'])]['country'] != "none":
 			await state.finish()
 			await message.reply(f'You`ve already set your location ({users_info[str(message["from"]["id"])]["country"]})', reply_markup=main_menu)
+			requests.put(urls['users-info'], json=users_info, headers=headers)
 		else:
 			await message.answer(emojize("Send me your location to know your country. :world_map:"), reply_markup=location_request)
+			requests.put(urls['users-info'], json=users_info, headers=headers)
 			await User.waiting_for_location.set()
-
-		with open('users-info.txt') as file:
-			users_info = json.load(file)
-		file.close()
 
 	if text == "Cancel.":
 		await state.finish()
@@ -129,17 +132,14 @@ async def new_name(message: types.Message, state: FSMContext):
 		await message.reply("What do you want now?", reply_markup=main_menu)
 		return
 	
-	with open('users-info.txt') as file:
-		users_info = json.load(file)
-	file.close()
+	req = requests.get(urls['users-info'], headers=headers)
+	users_info = json.loads(req.text)
 	
 	name = message['text']
 	
 	users_info[str(message['from']['id'])]['nick'] = name
 	
-	with open('users-info.txt', 'w') as file:
-		file.write(json.dumps(users_info))
-	file.close()
+	requests.put(urls['users-info'], json=users_info, headers=headers)
 	
 	await message.reply(emojize(f'Your name was successfully changed to {name}! :tada:'), reply_markup=main_menu)
 	
@@ -148,20 +148,17 @@ async def new_name(message: types.Message, state: FSMContext):
 @dp.message_handler(state=User.waiting_for_location, content_types=types.ContentTypes.LOCATION)
 async def message_for_start(message: types.Message, state: FSMContext):
 	try:		
-		with open('users-info.txt') as file:
-			users_info = json.load(file)
-		file.close()
+		req = requests.get(urls['users-info'], headers=headers)
+		users_info = json.loads(req.text)
 		
 		if users_info[str(message['from']['id'])]['country'] == "none":
 			country = get_place(message.location["latitude"], message.location["longitude"])
 			users_info[str(message['from']['id'])]['country'] = country
+			requests.put(urls['users-info'], json=users_info, headers=headers)
 			await message.reply(f"Congrats, cranes will fly to {country} and meet you soon.", reply_markup=main_menu)
 		else:
+			requests.put(urls['users-info'], json=users_info, headers=headers)
 			await message.reply(emojize("You can't change your location. :pensive:"), reply_markup=main_menu)
-		
-		with open('users-info.txt', 'w') as file:
-			file.write(json.dumps(users_info))
-		file.close()
 	except:
 		await message.reply(emojize("Sorry, but we have some troubles with your location right now, try later. :pensive:"), reply_markup=main_menu)
 		
@@ -175,15 +172,13 @@ async def choose_crane(message:types.Message, state: FSMContext):
 		await message.reply("What do you want now?", reply_markup=main_menu)
 		return
 	
-	with open("users-info.txt") as file:
-		users_info = json.load(file)
-	file.close()
+	req = requests.get(urls['users-info'], headers=headers)
+	users_info = json.loads(req.text)
 	
 	read_cranes = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	
-	with open('cranes.txt') as file:
-		cranes = json.load(file)
-	file.close()
+	req = requests.get(urls['cranes'], headers=headers)
+	cranes = json.loads(req.text)
 	
 	global to_share
 	to_share = {}
@@ -196,10 +191,16 @@ async def choose_crane(message:types.Message, state: FSMContext):
 	if message['text'] == 'Random crane.':
 		yes = random.choice(list(to_share.values()))
 		await message.reply(f'{cranes["cranes"][yes][0]}'+'\n\n'+emojize(f'{cranes["cranes"][yes][-1]}'), reply_markup=main_menu)
+		requests.put(urls['users-info'], json=users_info, headers=headers)
+		requests.put(urls['cranes'], json=cranes, headers=headers)
+		print(req.text)
 		await state.finish()
 	else:
 		await state.update_data(cranes_=to_share)
 		await message.answer('Choose a paper crane from menu.', reply_markup=read_cranes)
+		requests.put(urls['users-info'], json=users_info, headers=headers)
+		requests.put(urls['cranes'], json=cranes, headers=headers)
+		print(req.text)
 		await state.finish()
 		await Crane.dysplay_a_crane.set()
 
@@ -211,11 +212,12 @@ async def dysplay(message: types.Message, state: FSMContext):
 		await message.reply("What do you want now?", reply_markup=main_menu)
 		return
 	
-	with open('cranes.txt') as file:
-		cranes = json.load(file)
-	file.close()
+	req = requests.get(urls['cranes'], headers=headers)
+	cranes = json.loads(req.text)
 	
 	await message.reply(emojize(cranes['cranes'][to_share[message['text']]][-1]), reply_markup = main_menu)
+	
+	requests.put(urls['cranes'], json=cranes, headers=headers)
 	
 	await state.finish()
 
@@ -231,13 +233,11 @@ async def catch(message: types.Message, state: FSMContext):
 		await state.finish()
 		return
 	if message['text'] == 'Try.':
-		with open('users-info.txt') as file:
-			users_info = json.load(file)
-		file.close()
+		req = requests.get(urls['users-info'], headers=headers)
+		users_info = json.loads(req.text)
 		
-		with open('cranes.txt') as file:
-			cranes = json.load(file)
-		file.close()
+		req = requests.get(urls['cranes'], headers=headers)
+		cranes = json.loads(req.text)
 		
 		crane_to_catch = ''
 		
@@ -253,11 +253,11 @@ async def catch(message: types.Message, state: FSMContext):
 		
 		users_info[str(message['from']['id'])]['read-cranes'].append(crane_to_catch)
 		
-		with open('users-info.txt', 'w') as file:
-			file.write(json.dumps(users_info))
-		file.close()
+		requests.put(urls['users-info'], json=users_info, headers=headers)
 		
 		await message.reply(f'{cranes["cranes"][crane_to_catch][0]}'+'\n\n'+emojize(f'{cranes["cranes"][crane_to_catch][-1]}'), reply_markup=main_menu)
+		requests.put(urls['cranes'], json=cranes, headers=headers)
+		print(req.text)
 		await state.finish()
 	else:
 		await message.reply(emojize('Use only commands from menu, please! :sob:'))
@@ -292,13 +292,11 @@ async def crane_text(message: types.Message, state: FSMContext):
 		await message.reply("What do you want now?", reply_markup=main_menu)
 		return
 	
-	with open('users-info.txt') as file:
-		users_info = json.load(file)
-	file.close()
+	req = requests.get(urls['users-info'], headers=headers)
+	users_info = json.loads(req.text)
 	
-	with open('cranes.txt') as file:
-		cranes = json.load(file)
-	file.close()
+	req = requests.get(urls['cranes'], headers=headers)
+	cranes = json.loads(req.text)
 	
 	code = gen()
 	
@@ -310,13 +308,10 @@ async def crane_text(message: types.Message, state: FSMContext):
 	
 	cranes['cranes'][code] = [title, new_text]
 	
-	with open('users-info.txt', 'w') as file:
-		file.write(json.dumps(users_info))
-	file.close()
+	requests.put(urls['users-info'], json=users_info, headers=headers)
 	
-	with open('cranes.txt', 'w') as file:
-		file.write(json.dumps(cranes))
-	file.close()
+	requests.put(urls['cranes'], json=users_info, headers=headers)
+	print(req.text)
 	
 	await message.reply('Your paper crane was sent on the other side of Earth.', reply_markup=main_menu)
 	
